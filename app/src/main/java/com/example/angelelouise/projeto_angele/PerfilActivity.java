7c585d92fe713972590553f7e517f570f4cbde62
Angele Louise <angelealst@hotmail.com>
2018-05-05 02:39:49 -0300

    quase tudo funcionando, falta o serilizable e corrigir os bugs
    SharedPreferences Ok
    ExternalStorage OK

--- app/src/main/java/com/example/angelelouise/projeto_angele/PerfilActivity.java
+++ app/src/main/java/com/example/angelelouise/projeto_angele/PerfilActivity.java
@@ -2,6 +2,7 @@ package com.example.angelelouise.projeto_angele;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,27 +17,62 @@ import android.widget.TextView;
 public class PerfilActivity extends Activity{
 
     private Usuario usuario_principal;
+    TextView usuario;
+    TextView email;
+    TextView descricao;
+    TextView nome;
+    ImageView perfil;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        TextView usuario = findViewById(R.id.perfil_usuario);
-        TextView email = findViewById(R.id.perfil_email);
-        TextView descricao = findViewById(R.id.perfil_descricao);
-        TextView nome = findViewById(R.id.perfil_nome);
-        ImageView perfil = findViewById(R.id.perfil_img);
+        usuario = findViewById(R.id.perfil_usuario);
+        email = findViewById(R.id.perfil_email);
+        descricao = findViewById(R.id.perfil_descricao);
+        nome = findViewById(R.id.perfil_nome);
+        perfil = findViewById(R.id.perfil_img);
+
+        Intent intent = getIntent();
+        usuario_principal = (Usuario) intent.getExtras().get(Usuario.USUARIO_INFO);
 
         usuario.setText(usuario_principal.getLogin());
         email.setText(usuario_principal.getEmail());
         nome.setText(usuario_principal.getNome());
 
+        if (usuario_principal.getPerfil()!=null){
+            perfil.setImageBitmap(usuario_principal.getPerfil());
+        }
+
 
     }
 
     public void editar(View v){
-        Intent intent = new Intent(PerfilActivity.this, EditActivity.class);
-        startActivity(intent);
+        Intent editintent = new Intent(PerfilActivity.this, EditActivity.class);
+        editintent.putExtra(Usuario.USUARIO_INFO, usuario_principal);
+        startActivityForResult(editintent,1);
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && data != null) {
+            if (resultCode == RESULT_OK) {
+                usuario_principal = (Usuario) data.getExtras().get("data");
 
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(getIntent());
+                overridePendingTransition(0, 0);
+                email.setText(usuario_principal.getEmail());
+                nome.setText(usuario_principal.getNome());
+                descricao.setText(usuario_principal.getDescricao());
+                perfil.setImageBitmap(usuario_principal.getPerfil());
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        this.onCreate(null);
+    }
 }
